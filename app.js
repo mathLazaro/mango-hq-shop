de895a45d7e50b0f7e1bd4aea8e6abf0f48017ca
Matheus <matheus.lazaro2002@gmail.com>
2024-07-02 10:04:50 -0300

    configuração do cors na api

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import usuarioRouter from "./Routes/usuario.routes.js";
 import carrinhoRouter from "./Routes/carrinho.routes.js";
 import userLevel from "./middlewares/base.middleware.js";
 import { initAdmin, initLivros } from "./utils/init.js";
+import cors from "cors";
 
 import "dotenv/config"; // importando o dotenv para carregar as variáveis de ambiente
 
@@ -12,6 +13,7 @@ const app = express();
 const port = process.env.APP_PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/public", express.static("public"));
 
 app.use("/livros", livroRouter);
@@ -20,8 +22,8 @@ app.use("/usuarios", usuarioRouter);
 app.use("/carrinho", userLevel, carrinhoRouter);
 
 app.listen(port, () => {
-  initAdmin();
-  initLivros();
+    initAdmin();
+    initLivros();
 
-  console.log("Servidor rodando na porta " + port);
+    console.log("Servidor rodando na porta " + port);
 });
